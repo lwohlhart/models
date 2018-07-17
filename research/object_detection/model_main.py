f6e3821d33c0e0183a618e80811d989158823c32
Lucas Wohlhart <lucas@wohlhart.at>
2018-07-17 17:00:04 +0200

    remove absl dependency, use tf.flags

--- research/object_detection/model_main.py
+++ research/object_detection/model_main.py
@@ -18,9 +18,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl import flags
+# from absl import flags
 
 import tensorflow as tf
+
+flags = tf.app.flags
 
 from object_detection import model_hparams
 from object_detection import model_lib
